b8b4bebda0dd89aa399de3a5e0cd762fa6f98561
Patrick <patrick@godknows.de>
2021-04-27 10:58:14 +0200

    Add project files.

---
+++ BlazorBattles/Client/Pages/Counter.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Pages
+{
+    public partial class Counter : ComponentBase
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount += 2;
+        }
+    }
+}
