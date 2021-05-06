4fd5cf4b1ce7e72d143c1742fd1a8fede2db9d55
Patrick <patrick@godknows.de>
2021-05-06 16:04:46 +0200

    New Models for Knights & More

---
+++ BlazorBattles/Shared/Unit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Shared
+{
+    public class Unit
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int HitPoints { get; set; } = 100;
+        public int BananaCost { get; set; }
+    }
+}
---
+++ BlazorBattles/Shared/UserUnit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Shared
+{
+    public class UserUnit
+    {
+        public int UserId { get; set; }
+        public int UnitId { get; set; }
+        public int HitPoints { get; set; }
+    }
+}
