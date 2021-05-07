a67b7d96fbfd7d4ac65d346a6ce0d48d21c07655
Patrick <patrick@godknows.de>
2021-05-07 10:28:53 +0200

    User Login Form with Validation

---
+++ BlazorBattles/Shared/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Shared
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "Please enter a username.")]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
