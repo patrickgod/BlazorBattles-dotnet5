5eb25550b7751bfe3b19e203cbe88f682b4833c3
Patrick <patrick@godknows.de>
2021-05-10 20:59:53 +0200

    Add the UnitController

---
+++ BlazorBattles/Server/Controllers/UnitController.cs
@@ -0,0 +1,28 @@
+﻿using BlazorBattles.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UnitController : ControllerBase
+    {
+        public IList<Unit> Units => new List<Unit>
+        {
+            new Unit { Id = 1, Title = "Knight", Attack = 10, Defense = 10, BananaCost = 100},
+            new Unit { Id = 2, Title = "Archer", Attack = 15, Defense = 5, BananaCost = 150},
+            new Unit { Id = 3, Title = "Mage", Attack = 20, Defense = 1, BananaCost = 200}
+        };
+
+        [HttpGet]
+        public IActionResult GetUnits()
+        {
+            return Ok(Units);
+        }
+    }
+}
