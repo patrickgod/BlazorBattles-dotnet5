89d11aaf9b2c2f15fbdce15d47d931167dff682e
Patrick <patrick@godknows.de>
2021-05-11 21:33:33 +0200

    ConnectionString & Add the DbContext

--- BlazorBattles/Server/Startup.cs
+++ BlazorBattles/Server/Startup.cs
@@ -1,7 +1,9 @@
+using BlazorBattles.Server.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,7 +24,8 @@ namespace BlazorBattles.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
