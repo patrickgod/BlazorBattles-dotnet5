5476a351fff6cc357800b6aef0802452870f786a
Patrick <patrick@godknows.de>
2021-05-11 22:17:35 +0200

    Remove a Unit with DELETE

--- BlazorBattles/Server/Controllers/UnitController.cs
+++ BlazorBattles/Server/Controllers/UnitController.cs
@@ -55,5 +55,20 @@ namespace BlazorBattles.Server.Controllers
 
             return Ok(dbUnit);
         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUnit(int id)
+        {
+            var dbUnit = await _context.Units.FirstOrDefaultAsync(u => u.Id == id);
+            if (dbUnit == null)
+            {
+                return NotFound("Unit with the given Id doesn't exist.");
+            }
+
+            _context.Units.Remove(dbUnit);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Units.ToListAsync());
+        }
     }
 }
