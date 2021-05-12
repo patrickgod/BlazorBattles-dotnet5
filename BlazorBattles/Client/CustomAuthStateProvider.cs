6c3146efdca9e680c88632c8be0a767b6f3f7054
Patrick <patrick@godknows.de>
2021-05-12 21:31:08 +0200

    Use JWT with the Client

--- BlazorBattles/Client/CustomAuthStateProvider.cs
+++ BlazorBattles/Client/CustomAuthStateProvider.cs
@@ -3,7 +3,10 @@ using Microsoft.AspNetCore.Components.Authorization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Claims;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace BlazorBattles.Client
@@ -11,26 +14,53 @@ namespace BlazorBattles.Client
     public class CustomAuthStateProvider : AuthenticationStateProvider
     {
         private readonly ILocalStorageService _localStorageService;
+        private readonly HttpClient _http;
 
-        public CustomAuthStateProvider(ILocalStorageService localStorageService)
+        public CustomAuthStateProvider(ILocalStorageService localStorageService, HttpClient http)
         {
             _localStorageService = localStorageService;
+            _http = http;
         }
 
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            var state = new AuthenticationState(new ClaimsPrincipal());
-            if (await _localStorageService.GetItemAsync<bool>("isAuthenticated"))
-            {
-                var identity = new ClaimsIdentity(new[] {
-                    new Claim(ClaimTypes.Name, "Patrick")
-                }, "test authentication type");
+            string authToken = await _localStorageService.GetItemAsStringAsync("authToken");
 
-                var user = new ClaimsPrincipal(identity);
-                state = new AuthenticationState(user);
+            var identity = new ClaimsIdentity();
+            _http.DefaultRequestHeaders.Authorization = null;
+
+            if (!string.IsNullOrEmpty(authToken))
+            {
+                identity = new ClaimsIdentity(ParseClaimsFromJwt(authToken), "jwt");
+                _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken);
             }
+
+            var user = new ClaimsPrincipal(identity);
+            var state = new AuthenticationState(user);
+
             NotifyAuthenticationStateChanged(Task.FromResult(state));
+
             return state;
+        }
+
+        private byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2: base64 += "=="; break;
+                case 3: base64 += "="; break;
+            }
+            return Convert.FromBase64String(base64);
+        }
+
+        private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+            var claims = keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()));
+
+            return claims;
         }
     }
 }
