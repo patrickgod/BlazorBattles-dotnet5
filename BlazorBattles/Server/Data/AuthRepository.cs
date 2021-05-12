205c50b6b7e8b08545232b1570367ae6b611d5d3
Patrick <patrick@godknows.de>
2021-05-12 19:54:38 +0200

    User Registration

--- BlazorBattles/Server/Data/AuthRepository.cs
+++ BlazorBattles/Server/Data/AuthRepository.cs
@@ -2,25 +2,50 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace BlazorBattles.Server.Data
 {
     public class AuthRepository : IAuthRepository
     {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+
         public Task<string> Login(string email, string password)
         {
             throw new NotImplementedException();
         }
 
-        public Task<int> Register(User user, string password)
+        public async Task<int> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user.Id;
         }
 
         public Task<bool> UserExists(string email)
         {
             throw new NotImplementedException();
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
