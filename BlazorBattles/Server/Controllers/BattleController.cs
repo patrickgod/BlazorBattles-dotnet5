3f7ff576393a2c78c2443d7db488e212f17e0240
Patrick <patrick@godknows.de>
2021-05-14 20:44:23 +0200

    Create the BattleController

---
+++ BlazorBattles/Server/Controllers/BattleController.cs
@@ -0,0 +1,40 @@
+﻿using BlazorBattles.Server.Data;
+using BlazorBattles.Server.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class BattleController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly IUtilityService _utilityService;
+
+        public BattleController(DataContext context, IUtilityService utilityService)
+        {
+            _context = context;
+            _utilityService = utilityService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> StartBattle([FromBody] int opponentId)
+        {
+            var attacker = await _utilityService.GetUser();
+            var opponent = await _context.Users.FindAsync(opponentId);
+            if (opponent == null || opponent.IsDeleted)
+            {
+                return NotFound("Opponent not available.");
+            }
+
+            return Ok();
+        }
+    }
+}
