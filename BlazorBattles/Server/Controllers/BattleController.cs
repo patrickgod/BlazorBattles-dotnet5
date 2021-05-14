f9652c93f520909b2fc7b3a498bce1ce6fee10a7
Patrick <patrick@godknows.de>
2021-05-14 21:17:46 +0200

    Implement the Battle Logic

--- BlazorBattles/Server/Controllers/BattleController.cs
+++ BlazorBattles/Server/Controllers/BattleController.cs
@@ -1,8 +1,10 @@
 ﻿using BlazorBattles.Server.Data;
 using BlazorBattles.Server.Services;
+using BlazorBattles.Shared;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,7 +36,102 @@ namespace BlazorBattles.Server.Controllers
                 return NotFound("Opponent not available.");
             }
 
-            return Ok();
+            var result = new BattleResult();
+            await Fight(attacker, opponent, result);
+
+            return Ok(result);
+        }
+
+        private async Task Fight(User attacker, User opponent, BattleResult result)
+        {
+            var attackerArmy = await _context.UserUnits
+                .Where(u => u.UserId == attacker.Id && u.HitPoints > 0)
+                .Include(u => u.Unit)
+                .ToListAsync();
+            var opponentArmy = await _context.UserUnits
+                .Where(u => u.UserId == opponent.Id && u.HitPoints > 0)
+                .Include(u => u.Unit)
+                .ToListAsync();
+
+            var attackerDamageSum = 0;
+            var opponentDamageSum = 0;
+
+            int currentRound = 0;
+            while (attackerArmy.Count > 0 && opponentArmy.Count > 0)
+            {
+                currentRound++;
+
+                if (currentRound % 2 != 0)
+                    attackerDamageSum += FightRound(attacker, opponent, attackerArmy, opponentArmy, result);
+                else
+                    opponentDamageSum += FightRound(opponent, attacker, opponentArmy, attackerArmy, result);
+            }
+
+            result.IsVictory = opponentArmy.Count == 0;
+            result.RoundsFought = currentRound;
+
+            if (result.RoundsFought > 0)
+                await FinishFight(attacker, opponent, result, attackerDamageSum, opponentDamageSum);
+        }
+
+        private int FightRound(User attacker, User opponent,
+            List<UserUnit> attackerArmy, List<UserUnit> opponentArmy, BattleResult result)
+        {
+            int randomAttackerIndex = new Random().Next(attackerArmy.Count);
+            int randomOpponentIndex = new Random().Next(opponentArmy.Count);
+
+            var randomAttacker = attackerArmy[randomAttackerIndex];
+            var randomOpponent = opponentArmy[randomOpponentIndex];
+
+            var damage =
+                new Random().Next(randomAttacker.Unit.Attack) - new Random().Next(randomOpponent.Unit.Defense);
+            if (damage < 0) damage = 0;
+
+            if (damage <= randomOpponent.HitPoints)
+            {
+                randomOpponent.HitPoints -= damage;
+                result.Log.Add(
+                    $"{attacker.Username}'s {randomAttacker.Unit.Title} attacks " +
+                    $"{opponent.Username}'s {randomOpponent.Unit.Title} with {damage} damage.");
+                return damage;
+            }
+            else
+            {
+                damage = randomOpponent.HitPoints;
+                randomOpponent.HitPoints = 0;
+                opponentArmy.Remove(randomOpponent);
+                result.Log.Add(
+                    $"{attacker.Username}'s {randomAttacker.Unit.Title} kills " +
+                    $"{opponent.Username}'s {randomOpponent.Unit.Title}!");
+                return damage;
+            }
+        }
+
+        private async Task FinishFight(User attacker, User opponent, BattleResult result,
+            int attackerDamageSum, int opponentDamageSum)
+        {
+            result.AttackerDamageSum = attackerDamageSum;
+            result.OpponentDamageSum = opponentDamageSum;
+
+            attacker.Battles++;
+            opponent.Battles++;
+
+            if (result.IsVictory)
+            {
+                attacker.Victories++;
+                opponent.Defeats++;
+                attacker.Bananas += opponentDamageSum;
+                opponent.Bananas += attackerDamageSum * 10;
+            }
+            else
+            {
+                attacker.Defeats++;
+                opponent.Victories++;
+                attacker.Bananas += opponentDamageSum * 10;
+                opponent.Bananas += attackerDamageSum;
+            }
+
+            await _context.SaveChangesAsync();
         }
     }
 }
---
+++ BlazorBattles/Shared/BattleResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Shared
+{
+    public class BattleResult
+    {
+        public IList<string> Log { get; set; } = new List<string>();
+        public int AttackerDamageSum { get; set; }
+        public int OpponentDamageSum { get; set; }
+        public bool IsVictory { get; set; }
+        public int RoundsFought { get; set; }
+    }
+}
