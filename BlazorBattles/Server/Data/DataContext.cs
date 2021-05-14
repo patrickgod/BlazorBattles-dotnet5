21c3a492fd12c474c51ff6eb77ddd727b8216fd6
Patrick <patrick@godknows.de>
2021-05-14 21:54:12 +0200

    The Battle Model

--- BlazorBattles/Server/Data/DataContext.cs
+++ BlazorBattles/Server/Data/DataContext.cs
@@ -14,10 +14,30 @@ namespace BlazorBattles.Server.Data
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Battle>()
+                .HasOne(b => b.Attacker)
+                .WithMany()
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Battle>()
+                .HasOne(b => b.Opponent)
+                .WithMany()
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Battle>()
+                .HasOne(b => b.Winner)
+                .WithMany()
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+
         public DbSet<Unit> Units { get; set; }
 
         public DbSet<User> Users { get; set; }
 
         public DbSet<UserUnit> UserUnits { get; set; }
+
+        public DbSet<Battle> Battles { get; set; }
     }
 }
---
+++ BlazorBattles/Server/Migrations/20210514195306_Battles.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorBattles.Server.Migrations
+{
+    public partial class Battles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Battles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AttackerId = table.Column<int>(type: "int", nullable: false),
+                    OpponentId = table.Column<int>(type: "int", nullable: false),
+                    WinnerId = table.Column<int>(type: "int", nullable: false),
+                    WinnerDamage = table.Column<int>(type: "int", nullable: false),
+                    RoundsFought = table.Column<int>(type: "int", nullable: false),
+                    BattleDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Battles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Battles_Users_AttackerId",
+                        column: x => x.AttackerId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Battles_Users_OpponentId",
+                        column: x => x.OpponentId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Battles_Users_WinnerId",
+                        column: x => x.WinnerId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Battles_AttackerId",
+                table: "Battles",
+                column: "AttackerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Battles_OpponentId",
+                table: "Battles",
+                column: "OpponentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Battles_WinnerId",
+                table: "Battles",
+                column: "WinnerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Battles");
+        }
+    }
+}
