87cc51f3bedc5da15b7fcd708419046c01cb33aa
Patrick <patrick@godknows.de>
2021-05-14 10:26:45 +0200

    Implement the Leaderboard

--- BlazorBattles/Server/Controllers/UserController.cs
+++ BlazorBattles/Server/Controllers/UserController.cs
@@ -43,5 +43,30 @@ namespace BlazorBattles.Server.Controllers
             await _context.SaveChangesAsync();
             return Ok(user.Bananas);
         }
+
+        [HttpGet("leaderboard")]
+        public async Task<IActionResult> GetLeaderboard()
+        {
+            var users = await _context.Users.Where(user => !user.IsDeleted && user.IsConfirmed).ToListAsync();
+
+            users = users
+                .OrderByDescending(u => u.Victories)
+                .ThenBy(u => u.Defeats)
+                .ThenBy(u => u.DateCreated)
+                .ToList();
+
+            int rank = 1;
+            var response = users.Select(user => new UserStatistic
+            {
+                Rank = rank++,
+                UserId = user.Id,
+                Username = user.Username,
+                Battles = user.Battles,
+                Victories = user.Victories,
+                Defeats = user.Defeats
+            });
+
+            return Ok(response);
+        }
     }
 }
---
+++ BlazorBattles/Shared/UserStatistic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Shared
+{
+    public class UserStatistic
+    {
+        public int Rank { get; set; }
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public int Battles { get; set; }
+        public int Victories { get; set; }
+        public int Defeats { get; set; }
+    }
+}
