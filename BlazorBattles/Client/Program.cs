2e622691f93aa99c2c99112c435bbac49a5f8d6d
Patrick <patrick@godknows.de>
2021-05-17 12:07:26 +0200

    Fight Battles in Blazor WebAssembly

--- BlazorBattles/Client/Program.cs
+++ BlazorBattles/Client/Program.cs
@@ -31,6 +31,7 @@ namespace BlazorBattles.Client
             builder.Services.AddAuthorizationCore();
             builder.Services.AddScoped<IAuthService, AuthService>();
             builder.Services.AddScoped<ILeaderboardService, LeaderboardService>();
+            builder.Services.AddScoped<IBattleService, BattleService>();
 
             await builder.Build().RunAsync();
         }
---
+++ BlazorBattles/Client/Services/BattleService.cs
@@ -0,0 +1,26 @@
+﻿using BlazorBattles.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+    public class BattleService : IBattleService
+    {
+        private readonly HttpClient _http;
+
+        public BattleService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<BattleResult> StartBattle(int opponentId)
+        {
+            var result = await _http.PostAsJsonAsync("api/battle", opponentId);
+            return await result.Content.ReadFromJsonAsync<BattleResult>();
+        }
+    }
+}
