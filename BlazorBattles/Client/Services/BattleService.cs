d28366ab6d4b56e715bfe42e7dcaa920227b3518
Patrick <patrick@godknows.de>
2021-05-17 12:45:01 +0200

    Add the Battle History Page

--- BlazorBattles/Client/Services/BattleService.cs
+++ BlazorBattles/Client/Services/BattleService.cs
@@ -18,12 +18,19 @@ namespace BlazorBattles.Client.Services
         }
 
         public BattleResult LastBattle { get; set; } = new BattleResult();
+        public IList<BattleHistoryEntry> History { get; set; } = new List<BattleHistoryEntry>();
 
         public async Task<BattleResult> StartBattle(int opponentId)
         {
             var result = await _http.PostAsJsonAsync("api/battle", opponentId);
             LastBattle = await result.Content.ReadFromJsonAsync<BattleResult>();
             return LastBattle;
+        }
+
+        public async Task GetHistory()
+        {
+            History = await _http.GetFromJsonAsync<BattleHistoryEntry[]>("api/user/history");
+            Console.WriteLine();
         }
     }
 }
--- BlazorBattles/Client/Services/IBattleService.cs
+++ BlazorBattles/Client/Services/IBattleService.cs
@@ -9,6 +9,8 @@ namespace BlazorBattles.Client.Services
     public interface IBattleService
     {
         BattleResult LastBattle { get; set; }
+        IList<BattleHistoryEntry> History { get; set; }
         Task<BattleResult> StartBattle(int opponentId);
+        Task GetHistory();
     }
 }
